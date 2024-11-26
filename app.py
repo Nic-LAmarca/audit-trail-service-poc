771081f4ed824291361b84974218f9e5d24dc0d8
Nic L'Amarca <nicolaslamarca97@gmail.com>
2024-11-26 15:20:38 -0700

    write app.py + style fixes

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///audit_trail.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    audit_trail_db.init_app(app) 
+    
+    with app.app_context():
+        from . import routes 
+        audit_trail_db.create_all()
+        return app
 
-    audit_trail_db.init_app(app)
-
-    from .routes import main
-    app.register_blueprint(main)
-
-    return app
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
+    
--- app/models.py
+++ app/models.py
@@ -14,24 +14,24 @@ import uuid
 from . import audit_trail_db
 
 class AuditLogEvent(audit_trail_db.Model):
-    """ Represents an audit log entry for tracking events in the system. 
+    """ Represents an audit log entry for tracking events in the system.
     
-    Attributes: 
-    event_id (str): Unique identifier for the event in UUID format. If one is not provided, an UUID value will be automatically generated. 
-    event_type (str): Type of event (e.g., 'USER_LOGIN'). 
-    created_at (datetime): Time when the event occurred. 
-    client_id (str): Identifier for the client generating the event. 
-    user_id (str, optional): Identifier for the user performing the action. 
-    request_method (str): HTTP method used for the request. 
-    status (int, optional): Status code of the event captured by the audit log (e.g., 200). 
+    Attributes:
+    event_id (str): Unique identifier for the event in UUID format. If one is not provided, an UUID value will be automatically generated.
+    event_type (str): Type of event (e.g., 'USER_LOGIN').
+    created_at (datetime): Time when the event occurred.
+    client_id (str): Identifier for the client generating the event.
+    user_id (str, optional): Identifier for the user performing the action.
+    request_method (str): HTTP method used for the request.
+    status (int, optional): Status code of the event captured by the audit log (e.g., 200).
     entity_json (JSON, optional): the variant data for the event, specific to the given microservice logging it.
     """
     event_id = audit_trail_db.Column(audit_trail_db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()), nullable=False)
-    event_type = audit_trail_db.Column(audit_trail_db.String(50), nullable=False) 
-    created_at = audit_trail_db.Column(audit_trail_db.DateTime, default=lambda: datetime.utcnow(), nullable=False) 
-    client_id = audit_trail_db.Column(audit_trail_db.String(50), nullable=False) 
-    user_id = audit_trail_db.Column(audit_trail_db.String(50), nullable=True) 
-    request_method = audit_trail_db.Column(audit_trail_db.String(10), nullable=False) 
-    status = audit_trail_db.Column(audit_trail_db.Integer, nullable=True) 
+    event_type = audit_trail_db.Column(audit_trail_db.String(50), nullable=False)
+    created_at = audit_trail_db.Column(audit_trail_db.DateTime, default=lambda: datetime.utcnow(), nullable=False)
+    client_id = audit_trail_db.Column(audit_trail_db.String(50), nullable=False)
+    user_id = audit_trail_db.Column(audit_trail_db.String(50), nullable=True)
+    request_method = audit_trail_db.Column(audit_trail_db.String(10), nullable=False)
+    status = audit_trail_db.Column(audit_trail_db.Integer, nullable=True)
     entity_json = audit_trail_db.Column(audit_trail_db.JSON, nullable=True)
     
